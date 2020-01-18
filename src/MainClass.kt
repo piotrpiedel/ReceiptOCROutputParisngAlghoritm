a3c7007e3ef2d6f2c0442b5c8ad770e8e300e886
piotrpiedel <piotrpiedel96@gmail.com>
2020-01-18 01:37:23 +0100

    tokenizer with price and letter separately;

--- src/MainClass.kt
+++ src/MainClass.kt
@@ -2,7 +2,7 @@ import gdrive.GDriveResponseParserHelper
 import resources.receipt1
 
 fun main() {
-    GDriveResponseParserHelper().parseStringFromOcrToListOfOperations(resources.receipt3).forEach{
+    GDriveResponseParserHelper().parseStringFromOcrToListOfOperations(resources.receipt1).forEach{
         println(it)
     }
 }
---
+++ src/gdrive/TokensToListRebexPriceAndRegexletterMatcher.kt
@@ -0,0 +1,5 @@
+package gdrive
+
+//TODO: do this like match price normally but check if after regex next token is letter;
+class TokensToListRebexPriceAndRegexletterMatcher {
+}
